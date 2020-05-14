eac0a3420c8c468084fd09bbd48485b854e4449c
420343878@qq.com <420343878@qq.com>
2020-05-14 18:11:56 +0800

    add default value

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * @Author: xiaolong.qiu
  * @Date: 2020-05-14 17:28:01
  */
-module.exports = function(phone, rule){
+module.exports = function(phone, start, length, symbolChar, rule){
     var reg = rule || /^1([38][0-9]|4[579]|5[0-3,5-9]|6[6]|7[0135678]|9[89])\d{8}$/;
     // 验证号码合法性
     if( !(reg.test(phone)) ){
@@ -10,6 +10,6 @@ module.exports = function(phone, rule){
     }
 
     var arr = [...phone]
-    arr.splice(3, 4, "****")
+    arr.splice(start || 3, length || 4, symbolChar || "****")
     return arr.join("")
 }
